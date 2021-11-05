2b0bab5338d3230e65c1bca883a80b5aa1f576f3
Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>
2021-11-05 10:36:04 +0000

    Add new script
    Signed-off-by: Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>

--- Dep-Generator/Run-Others.sh
+++ Dep-Generator/Run-Others.sh
@@ -1,46 +1,58 @@
+## Tree Variations
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Default-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Default-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Default-PT/ 
 
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Default/ -n B2W-Polarity -s Train -d 300 -c 20 -l en -g tree_only;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Default/ -n B2W-Recommend -s Train -d 300 -c 20 -l en -g tree_only;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Default/ -n B2W-Rating -s Train -d 300 -c 20 -l en -g tree_only;
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-PT/ 
 
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order/ -n B2W-Polarity -s Train -d 300 -c 20 -l en -g tree_and_order;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order/ -n B2W-Recommend -s Train -d 300 -c 20 -l en -g tree_and_order;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order/ -n B2W-Rating -s Train -d 300 -c 20 -l en -g tree_and_order;
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-PT/ 
+
+## Node TAGS Datasets
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-Dep-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-Tag-Dep-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-Dep-PT/ 
+
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-POS-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-Tag-POS-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-POS-PT/ 
+
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-DEP_POS-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-Tag-DEP_POS-PT/ 
+mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-DEP_POS-PT/ 
 
 
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph/ -n B2W-Polarity -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order-Multigraph/ -n B2W-Recommend -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph/ -n B2W-Rating -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph;
+
+echo ">>> Building Tree Only"
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Default-PT/ -n B2W-Polarity -s Train -d 300 -c 20 -l pt -g tree_only;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Default-PT/ -n B2W-Recommend -s Train -d 300 -c 20 -l pt -g tree_only;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Default-PT/ -n B2W-Rating -s Train -d 300 -c 20 -l pt -g tree_only;
+
+echo ">>> Building Tree and Order"
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-PT/ -n B2W-Polarity -s Train -d 300 -c 20 -l pt -g tree_and_order;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-PT/ -n B2W-Recommend -s Train -d 300 -c 20 -l pt -g tree_and_order;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-PT/ -n B2W-Rating -s Train -d 300 -c 20 -l pt -g tree_and_order;
+
+echo ">>> Building Multigraph"
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-PT/ -n B2W-Polarity -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-PT/ -n B2W-Recommend -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-PT/ -n B2W-Rating -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph;
 
 
+echo ">>> Building DEP Tag"
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-Dep-PT/ -n B2W-Polarity -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t dep;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-Tag-Dep-PT/ -n B2W-Recommend -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t dep;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-Dep-PT/ -n B2W-Rating -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t dep;
 
-mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-Dep/ 
-mkdir ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order-Multigraph-Tag-Dep/ 
-mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-Dep/ 
-mkdir ../Datasets/MR/U2GNN-TREE-ORDER-MULTIGRAPH-Tag-Dep/ 
-mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-POS/ 
-mkdir ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order-Multigraph-Tag-POS/ 
-mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-POS/ 
-mkdir ../Datasets/MR/U2GNN-TREE-ORDER-MULTIGRAPH-Tag-POS/ 
-mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-DEP_POS/ 
-mkdir ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order-Multigraph-Tag-DEP_POS/ 
-mkdir ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-DEP_POS/ 
-mkdir ../Datasets/MR/U2GNN-TREE-ORDER-MULTIGRAPH-Tag-DEP_POS/ 
+echo ">>> Building POS Tag"
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-POS-PT/ -n B2W-Polarity -s Train -d 300 -c 1 -l pt -g tree_and_order_multi_graph -t pos;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-Tag-POS-PT/ -n B2W-Recommend -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t pos;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-POS-PT/ -n B2W-Rating -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t pos;
 
-echo "Building DEP Tag"
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-Dep/ -n B2W-Polarity -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order-Multigraph-Tag-Dep/ -n B2W-Recommend -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-Dep/ -n B2W-Rating -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep;
-# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/U2GNN-TREE-ORDER-MULTIGRAPH-Tag-Dep/ -n MR -s Train -d 300 -c 4 -l en -g tree_and_order_multi_graph -t dep;
-
-echo "Building POS Tag"
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-POS/ -n B2W-Polarity -s Train -d 300 -c 1 -l en -g tree_and_order_multi_graph -t pos;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order-Multigraph-Tag-POS/ -n B2W-Recommend -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t pos;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-POS/ -n B2W-Rating -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t pos;
-# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/U2GNN-TREE-ORDER-MULTIGRAPH-Tag-POS/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t pos;
-
-echo "Building DEP-POS Tag"
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-DEP_POS/ -n B2W-Polarity -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep-pos;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/2W_COMPLETE-Recommend-Order-Multigraph-Tag-DEP_POS/ -n B2W-Recommend -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep-pos;
-# python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-DEP_POS/ -n B2W-Rating -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep-pos;
-# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/U2GNN-TREE-ORDER-MULTIGRAPH-Tag-DEP_POS/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep-pos;
+echo ">>> Building DEP-POS Tag"
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Polarity-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Polarity-Order-Multigraph-Tag-DEP_POS-PT/ -n B2W-Polarity -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t dep-pos;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Recommend-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Recommend-Order-Multigraph-Tag-DEP_POS-PT/ -n B2W-Recommend -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t dep-pos;
+python main.py -i ../Datasets/B2W-PT/Raw/B2W-Rating-Balanced/ -o ../Datasets/B2W-PT/B2W_COMPLETE-Rating-Order-Multigraph-Tag-DEP_POS-PT/ -n B2W-Rating -s Train -d 300 -c 20 -l pt -g tree_and_order_multi_graph -t dep-pos;
