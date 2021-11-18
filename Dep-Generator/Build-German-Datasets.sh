f3adfeb1ef807c4c5f591875d2df81e204fbe00d
Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>
2021-11-18 17:11:55 +0000

    Add new scripts

--- Dep-Generator/Build-German-Datasets.sh
+++ Dep-Generator/Build-German-Datasets.sh
@@ -13,26 +13,26 @@ mkdir ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-SQRT-PROD/
 
 
 echo ">> Building Tree [1/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_only -t none
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_only -t none
 
 echo ">> Building Tree-Order [2/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_and_order -t none
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_and_order -t none
 
 echo ">> Building Tree-Order-Multigraph [3/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_and_order_multi_graph -t none
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_and_order_multi_graph -t none
 
 echo ">> Building Tree-Order-Multigraph-DEP [4/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-DEP/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_and_order_multi_graph -t dep
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-DEP/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_and_order_multi_graph -t dep
 
 echo ">> Building Tree-Order-Multigraph-POS [5/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-POS/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_and_order_multi_graph -t pos
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-POS/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_and_order_multi_graph -t pos
 
 echo ">> Building Tree-Order-Multigraph-DEP-POS [6/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-DEP-POS/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_and_order_multi_graph -t dep-pos
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-DEP-POS/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_and_order_multi_graph -t dep-pos
 
 echo ">> Building Tree-Order-Multigraph-POS-DEP [7/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-POS-DEP/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_and_order_multi_graph -t pos-dep
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-POS-DEP/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_and_order_multi_graph -t pos-dep
 
 echo ">> Building Tree-Order-Multigraph-SQRT-PROD [8/8]"
-python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-SQRT-PROD/ -n 10kGNAD -s Train -d 300 -c 8 -l de -g tree_and_order_multi_graph -t sqrt_product
+python main.py -i ../Datasets/10kGNAD-DE/Parsed/ -o ../Datasets/10kGNAD-DE/Tree-Order-Multigraph-SQRT-PROD/ -n 10kGNAD -s Train -d 300 -c 20 -l de -g tree_and_order_multi_graph -t sqrt_product
 
