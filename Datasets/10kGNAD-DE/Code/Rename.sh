d44f9c784cb8b17180dc4a43565e5f97c2d69aa4
Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>
2021-11-29 18:04:03 +0000

    Oraganize directory
    Signed-off-by: Henrique V. Ehrenfried <hvehrenfried@inf.ufpr.br>

---
+++ Datasets/10kGNAD-DE/Code/Rename.sh
@@ -0,0 +1,8 @@
+mv ../Tree/Train_10kGNAD.txt ../Tree/Tree.txt
+mv ../Tree-Order/Train_10kGNAD.txt ../Tree-Order/Tree-Order.txt
+mv ../Tree-Order-Multigraph/Train_10kGNAD.txt ../Tree-Order-Multigraph/Tree-Order-Multigraph.txt
+mv ../Tree-Order-Multigraph-DEP/Train_10kGNAD.txt ../Tree-Order-Multigraph-DEP/Tree-Order-Multigraph-DEP.txt
+mv ../Tree-Order-Multigraph-POS/Train_10kGNAD.txt ../Tree-Order-Multigraph-POS/Tree-Order-Multigraph-POS.txt
+mv ../Tree-Order-Multigraph-DEP-POS/Train_10kGNAD.txt ../Tree-Order-Multigraph-DEP-POS/Tree-Order-Multigraph-DEP-POS.txt
+mv ../Tree-Order-Multigraph-POS-DEP/Train_10kGNAD.txt ../Tree-Order-Multigraph-POS-DEP/Tree-Order-Multigraph-POS-DEPin_10kGNAD.txt
+mv ../Tree-Order-Multigraph-SQRT-PROD/Train_10kGNAD.txt ../Tree-Order-Multigraph-SQRT-PROD/Tree-Order-Multigraph-SQRT-PROD.txt
---
+++ Dep-Generator/Scripts/Build-MR.sh
@@ -0,0 +1,38 @@
+## Tree Variations
+mkdir ../Datasets/MR/MR-Default/ 
+mkdir ../Datasets/MR/MR-Order/ 
+mkdir ../Datasets/MR/MR-Order-Multigraph/ 
+
+
+## Node TAGS Datasets
+mkdir ../Datasets/MR/MR-Order-Multigraph-Tag-Dep/ 
+mkdir ../Datasets/MR/MR-Order-Multigraph-Tag-POS/ 
+mkdir ../Datasets/MR/MR-Order-Multigraph-Tag-DEP_POS/ 
+mkdir ../Datasets/MR/MR-Order-Multigraph-Tag-POS_DEP/
+mkdir ../Datasets/MR/MR-Order-Multigraph-Tag-SQRT_PROD/
+
+# echo ">>> Building Tree Only"
+# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Default/ -n MR -s Train -d 300 -c 20 -l en -g tree_only;
+
+# echo ">>> Building Tree and Order"
+# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Order/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order;
+
+# echo ">>> Building Multigraph"
+# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Order-Multigraph/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph;
+
+# echo ">>> Building DEP Tag"
+# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Order-Multigraph-Tag-Dep/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep;
+
+# echo ">>> Building POS Tag"
+# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Order-Multigraph-Tag-POS/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t pos;
+
+echo ">>> Building DEP-POS Tag"
+python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Order-Multigraph-Tag-DEP_POS/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t dep-pos;
+
+echo ">>> Building POS-DEP Tag"
+python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Order-Multigraph-Tag-POS_DEP/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t pos-dep;
+
+# echo ">>> Building SQRT-PROD Tag"
+# python main.py -i ../Datasets/MR/Parsed/Train/ -o ../Datasets/MR/MR-Order-Multigraph-Tag-SQRT_PROD/ -n MR -s Train -d 300 -c 20 -l en -g tree_and_order_multi_graph -t sqrt_product;
+
+
